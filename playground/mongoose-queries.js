0fd3a2d19cec8d0cf0934bc8a7cebf5b67ae5b26
KunYou Wang <stam987654@gmail.com>
2017-11-27 15:26:08 +0800

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+const {User} = require('../server/models/user');
+
+// var id = '5a1baed7506ec7fc4cb6af2c';
+// 
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+// 
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found'); //驗證Collection是否存在id(id要先符合規則)
+//   }
+//   console.log('Todo', todo);
+// }).catch((e) => console.log(e)); //id不符合規則(如id過長或過短)
+
+User.findById('5a1a64f6ce81381445b4ccf8').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
